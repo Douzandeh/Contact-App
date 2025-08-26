afffe8d042b398ddc9bb883f458f9164912cf039
Hossein Douzandeh <douzandeh.k@gmail.com>
2025-08-26 19:21:02 +0330

    feat(contacts): create Contacts component and render data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Contacts from "./components/Contacts";
 import Heade from "./components/Heade";
 
 function App() {
   return (
     <>
       <Heade />
+      <Contacts />
     </>
   );
 }
